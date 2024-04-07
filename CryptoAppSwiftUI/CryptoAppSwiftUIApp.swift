3e17d12a6849bed20e8ff860a28a9f37592b8a63
narayanshettigar <narayanshettigar16@gmail.com>
2024-04-07 12:22:45 +0530

    Initial Commit

---
+++ CryptoAppSwiftUI/CryptoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppSwiftUIApp.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Narayan Shettigar on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
